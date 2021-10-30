45761dba6d44f0145cebe4615a02ee9e6615c193
m_khorosh <marina.khoroshilova@gmail.com>
2021-10-30 22:28:56 +0300

    add task3

---
+++ task3/source.js
@@ -0,0 +1,30 @@
+function calculateTop() {
+    let str = document.getElementById("input_text").value;
+    if (!isNaN(str)) {  // type check
+        alert('Please enter text :)');
+    } else {
+        // remove special symbols, don't mind register
+        str = str.replace(/\n/g,' ');
+        str = str.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()]/g, " ").toLowerCase();
+        str = str.replace(/^\s+|\s+$|\s+(?=\s)/g, "");
+        let wordsArray = str.split(' ');
+        let dictionary = {};
+        for (let word of wordsArray) {
+            if (isNaN(word)) {  // if find a number in text -  don't count
+                (word in dictionary) ? dictionary[word]++ : dictionary[word] = 1;
+            }
+        }
+        let max = new Set(Object.values(dictionary));
+        let maxs = Array.from(max).sort(function (a, b) {return b - a}).slice(0, 3);
+        let answerString = '';
+        for (let i in maxs) {
+            let keys = Object.keys(dictionary).filter(k => dictionary[k] === maxs[i]);
+            answerString +='\n'+'Top ' + (+i + 1) + ':'+'\n';
+            for (let elem of keys) {
+                answerString+=elem + '    ';
+            }
+            answerString+='\n'+'(' + maxs[i] + ' times)';
+        }
+        alert(answerString);
+    }
+}
